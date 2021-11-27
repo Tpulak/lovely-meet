435087d644d319ab21a8e2607ffdac851be1fe04
Wildline <Wildlinelincifort11234@gmail.com>
2021-11-27 18:09:54 -0500

    update

---
+++ LovelyMeet/LovelyMeet/RegistrationsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RegistrationsViewController.swift
+//  LovelyMeet
+//
+//  Created by Wildline  Lincifort on 11/27/21.
+//
+
+import UIKit
+
+class RegistrationsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var FullNameLabel: UILabel!
+    @IBOutlet weak var FNTxt: UITextField!
+    @IBOutlet weak var AgeLabel: UILabel!
+    @IBOutlet weak var AgeTxt: UITextField!
+    @IBOutlet weak var GenderLabel: UILabel!
+    @IBOutlet weak var GenderTxt: UITextField!
+    @IBOutlet var SexOrientatinLabel: UIView!
+    @IBOutlet weak var SexOrientationTxt: UITextField!
+    @IBOutlet weak var BirthLabel: UILabel!
+    @IBOutlet weak var BirthdayTxt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
